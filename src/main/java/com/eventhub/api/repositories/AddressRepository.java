a4fdc4e85b002d456eabcd044f4facae6bb6acc6
João Lacerda <joaolacerda1862@gmail.com>
2025-03-07 10:19:13 -0300

    feat: add initial repositories

---
+++ src/main/java/com/eventhub/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.eventhub.api.repositories;
+
+import com.eventhub.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/eventhub/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.eventhub.api.repositories;
+
+import com.eventhub.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
